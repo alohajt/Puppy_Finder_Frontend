0d0d087be8c6db2f0b538a6dc5cd56e5c07183c5
alohajt <alohajiatao@gmail.com>
2020-03-26 13:30:08 -0600

    changes

--- showPuppies.js
+++ showPuppies.js
@@ -1,3 +1,11 @@
+const username = localStorage.getItem('username')
+header = document.querySelector('header')
+const userLocation = localStorage.getItem('location')
+
+const welcome_message = document.createElement('h1')
+welcome_message.innerHTML = `${username}'s homepage`
+header.append(welcome_message)
+
 fetch('http://localhost:3000/animals/')
     .then(response => response.json())
     .then(puppies => showPuppies(puppies))
@@ -5,29 +13,35 @@ fetch('http://localhost:3000/animals/')
 function showPuppies(puppies){
     const puppiesList = document.querySelector('#puppies-list')
 
+    // puppiesUnderLocation = puppies.select(puppy =>{
+    
+    //     puppy.location == userLocation
+    // })
 
     puppies.forEach(puppy => {
         const newPuppyCard = document.createElement('div')
         newPuppyCard.class="card"
         newPuppyCard.innerHTML = `
         <div class="card">
-            <p><a href='http://localhost:3001/showPuppy.html?id=${puppy.id}'>${puppy.name}</a></p>
-            <img src="${puppy.picture}" alt="picture of ${puppy.name}" style="width:100%">
-            <div class="container">
-                <h4><b>${puppy.bio}</b></h4>
+            <p class="card-title"><a href='http://localhost:3001/showPuppy.html?id=${puppy.id}'>${puppy.name}</a></p>
+            <div class="card-imgae">
+                <img src="${puppy.picture}" alt="picture of ${puppy.name}" style="width:60%">
             </div>
-            <form>
+            <div class="card-box">
+                <ul>Breed: ${puppy.breed}</ul>
+                <ul>Good with kids: ${puppy.gwkids}</ul>
+                <ul>Good with other dogs: ${puppy.gwdogs}</ul>
+                <ul>Good with other cats: ${puppy.gwcats}</ul>
+                <ul><b>${puppy.bio}</b></ul>
+            </div>
+            <form class="add-favorite">
                 <input id="${puppy.id}" type="button" value="add to favorites" onClick="addToFavorites()">
             </form>
         </div>`
         puppiesList.append(newPuppyCard)
     })
 }
-const username = localStorage.getItem('username')
-header = document.querySelector('header')
-const welcome_message = document.createElement('h1')
-welcome_message.innerHTML = `${username}'s homepage`
-header.append(welcome_message)
+
 
 function addToFavorites(){
 
